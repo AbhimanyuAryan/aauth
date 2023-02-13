d90d7b320404c445c77bd17fbaa42d6cb3ffce49
Abhi <8083613+AbhimanyuAryan@users.noreply.github.com>
2023-02-14 00:19:55 +0530

    ✨ register/login for user

--- start/routes.ts
+++ start/routes.ts
@@ -19,7 +19,13 @@
 */
 
 import Route from '@ioc:Adonis/Core/Route'
+import AuthController from 'App/Controllers/Http/AuthController'
 
 Route.get('/', async ({ view }) => {
   return view.render('welcome')
 })
+
+Route.get('register', 'AuthController.registerShow').as('auth.register.show')
+Route.post('register', 'AuthController.register').as('auth.register')
+Route.get('login', 'AuthController.loginShow').as('auth.login.show')
+Route.post('login', 'AuthController.login').as('auth.login')
